0120308ab9f99a02e9368159589a68222c40ee28
William Escamilla <wescamilla10@gmail.com>
2020-04-01 15:43:34 -0400

    Add files via upload

---
+++ src/trees/Pair.java
@@ -0,0 +1,33 @@
+package basics;
+
+public class Pair<E,F> {
+	// data fields
+	private E x;
+	private F y;
+	
+	public Pair(E x, F y) {
+		super();
+		this.x = x;
+		this.y = y;
+	}
+	public E getX() {
+		return x;
+	}
+	public void setX(E x) {
+		this.x = x;
+	}
+	public F getY() {
+		return y;
+	}
+	public void setY(F y) {
+		this.y = y;
+	}
+	@Override
+	public String toString() {
+		return "Pair [x=" + x + ", y=" + y + "]";
+	}
+	
+	
+	
+	
+}
